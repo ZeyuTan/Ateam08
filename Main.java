8c8efd26edd0be4c80b5bffe8d986bedad5037de
Samuel Weng <56089956+M1HuYo@users.noreply.github.com>
2019-12-01 17:33:07 -0600

    implemented some methods in Main.java

--- Main.java
+++ Main.java
@@ -4,108 +4,163 @@
 package application;
 
 import java.util.List;
-
 import javafx.application.Application;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.Label;
+import javafx.scene.control.Menu;
+import javafx.scene.control.MenuBar;
+import javafx.scene.control.MenuItem;
+import javafx.scene.control.TextField;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
-
 /**
- * @author lishuji998
+ * @author samue
  *
  */
 public class Main extends Application {
-	// store any command-line arguments that were entered.
-		// NOTE: this.getParameters().getRaw() will get these also
-		private List<String> args;
+    private List<String> args;
+    private static final int WINDOW_WIDTH = 500;
+    private static final int WINDOW_HEIGHT = 300;
+    private static final String APP_TITLE = "Social Network";
+    BorderPane root = new BorderPane();
 
-		private static final int WINDOW_WIDTH = 400;
-		private static final int WINDOW_HEIGHT = 400;
-		private static final String APP_TITLE = "aTeam8 NB!!";
-		
-		
-	@Override
-	public void start(Stage primaryStage) throws Exception {
-		// TODO Auto-generated method stub
-		// save args example
-		args = this.getParameters().getRaw();
-		
-	}
-	
-	/*
-	 * method to setup signUpBox
-	 */
-	private void setUpSignUpBox() {
-		
-	}
-	
-	/*
-	 * method to setup twoinputBox
-	 */
-	private void setUpTwoInputBox() {
-		
-	}
-	
-	/*
-	 * method to setup centerbox
-	 */
-	private void setUpCenterBox() {
-		
-	}
-	
-	/*
-	 * method to setup bottom box
-	 */
-	private void setUpBottomBox() {
-		
-	}
-	
-	/*
-	 * method to setup menu box
-	 */
-	private void setUpMenuBox() {
-		
-	}
-	
-	/*
-	 * method to draw a graph
-	 */
-	private void drawGraph(Graph graph) {
-		
-	}
-	
-	/*
-	 * method to draw Node
-	 */
-	private void drawNode(Graph graph,String name, double x, double y) {
-		
-	}
-	
-	/*
-	 * method to draw edge
-	 */
-	private void drawEdge(Graph graph,double x1, double x2, double y1, double y2) {
-		
-	}
-	
-	/*
-	 * method to get name by xy coordinate
-	 */
-	private void getNameFromCoordinate(double x, double y) {
-		
-	}
-	
-	/*method
-	 * to set the user the mouse clicked
-	 */
-	private void setSelectedUser(String str) {
-		
-	}
+    
+    VBox signUpBox = new VBox();
+    VBox twoInputBox = new VBox();
+    MenuBar menuBar = new MenuBar();  
+    
+    /*
+     * method to setup signUpBox
+     */
+    public void setUpSignUpBox() {
+        HBox hb1 = new HBox();
+        HBox hb2 = new HBox();
+        TextField tf1 = new TextField ();
+        TextField tf2 = new TextField ();
+        Label l1 = new Label("Username");
+        Label l2 = new Label("Password");
+        hb1.getChildren().addAll(l1, tf1);
+        hb2.getChildren().addAll(l2,tf2);
+        Button bt = new Button("Sign Up");
+        signUpBox.getChildren().addAll(hb1, hb2, bt);
+    }
+    
+    /*
+     * method to setup twoinputBox
+     */
+    public void setUpTwoInputBox() {
+        HBox hb1 = new HBox();
+        HBox hb2 = new HBox();
+        TextField tf1 = new TextField ();
+        TextField tf2 = new TextField ();
+        Label l1 = new Label("Info");
+        Label l2 = new Label("Info");
+        hb1.getChildren().addAll(l1, tf1);
+        hb2.getChildren().addAll(l2,tf2);
+        Button bt = new Button("Done");
+        twoInputBox.getChildren().addAll(hb1, hb2, bt);
+    }
+    
+    /*
+     * method to setup centerbox
+     */
+    public void setUpCenterBox() {
+        root.setCenter(new Label("(Social Network Graph)"));
+    }
+    
+    /*
+     * method to setup bottom box
+     */
+    public void setUpBottomBox() {
+        root.setBottom(new Label("(Bottom part content)"));
+    }
+    
+    /*
+     * method to setup menu box
+     */
+    public void setUpMenuBox() {
+        Menu file = new Menu("File");
+        MenuItem file1 = new MenuItem("Load");
+        MenuItem file2 = new MenuItem("Save");
+        Menu action = new Menu("Action");
+        MenuItem action1 = new MenuItem("Add Person");
+        MenuItem action2 = new MenuItem("Remove Person");
+        MenuItem action3 = new MenuItem("Add Relationship");
+        MenuItem action4 = new MenuItem("Remove Relationship");
+        file.getItems().addAll(file1, file2);
+        action.getItems().addAll(action1, action2, action3, action4);
+        menuBar.getMenus().addAll(file, action);
+    }
 
-	/**
-	 * @param args
-	 */
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		launch(args);
-	}
+
+    /*
+     * method to draw a graph
+     */
+//    private void drawGraph(Graph graph) {
+//        
+//    }
+    
+    /*
+     * method to draw Node
+     */
+//    private void drawNode(Graph graph,String name, double x, double y) {
+//        
+//    }
+    
+    /*
+     * method to draw edge
+     */
+//    private void drawEdge(Graph graph,double x1, double x2, double y1, double y2) {
+//        
+//    }
+//    
+    /*
+     * method to get name by xy coordinate
+     */
+//    private void getNameFromCoordinate(double x, double y) {
+//        
+//    }
+    
+    /*method
+     * to set the user the mouse clicked
+     */
+//    private void setSelectedUser(String str) {
+//        
+//    }
+    public void start(Stage primaryStage) {
+        args = this.getParameters().getRaw();
+        setUpSignUpBox();
+        setUpTwoInputBox();
+        setUpMenuBox();
+        root.setLeft(menuBar);
+        root.setRight(signUpBox);
+        setUpCenterBox();
+        setUpBottomBox();
+        Scene mainScene = new Scene(root, WINDOW_WIDTH, WINDOW_HEIGHT);
+        primaryStage.setTitle(APP_TITLE);
+        primaryStage.setScene(mainScene);
+        primaryStage.show();
+    }
+    
+
+    // Add the stuff and set the primary stage
+    
+    /**
+     * @param args
+     */
+    public static void main(String[] args) {
+        // TODO Auto-generated method stub
+        launch(args);
+    }
 
 }
